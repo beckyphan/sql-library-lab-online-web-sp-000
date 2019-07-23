171f350d19fa42400cce610a17461e121d56ff54
Becky Phan <beckyphan@live.com>
2019-07-23 02:19:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT name, COUNT("
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species ASC
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
