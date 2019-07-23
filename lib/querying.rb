73f4259f8838a2f73f6c30bbb91197790f27d081
Becky Phan <beckyphan@live.com>
2019-07-23 02:30:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM series
+  FROM authors
   INNER JOIN subgenres
   ON Authors.series_id= Series.id"
 end
